527ad70df7dd335e631e41ea24b90efdaf18d48f
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2025-10-11 18:39:27 -0600

    Añadiendo diseño generico y quiz generico

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const path = require('path');
+app.use(express.static('Public'));
+app.use(express.static(path.join(__dirname, 'src')));
 
-app.use(express.static('public'));
-
+// Ruta principal
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+app.get('/quiz', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/pages/quiz.html'));
 });
 
 app.listen(PORT, () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.{html,js}",
+    "./Public/**/*.{html,js}",
     "./src/**/*.{html,js}"
   ],
   theme: {
