e4526e30e5831023b50d26ee5f6ef6bee69e4b0d
Neil Otniel Moreno Rivera <neilotniel@gmail.com>
2025-11-30 22:28:27 -0600

    Prueba 2

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(express.static(path.join(__dirname, 'src')));
 
 // Ruta principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  //res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(process.cwd(), 'src/index.html'));
 });
 
 app.use('/pages', express.static(path.join(__dirname, 'src', 'Pages')));
